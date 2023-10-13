bd1c14ea7cd5eed248766dfca4274bfcc5b70d1d
DaniBacheva <y.bacheva@abv.bg>
2023-10-13 13:58:07 +0300

    login, register POST

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,8 +4,28 @@ router.get('/register', (req,res)=>{
     res.render('users/register')
 });
 
+router.post ('/register', (req, res) => {
+    const { firstNAme, lastName, email, password, repeatPassword} = req.body;
+
+    res.redirect('/users/login')
+
+    console.log(req.body)
+}
+)
+
 router.get('/login', (req,res)=> {
     res.render('users/login')
 });
+
+router.post ('/login', (req, res) => {
+    const { email, password} = req.body;
+
+    console.log(req.body)
+res.redirect('/')
+
+}
+)
+
+
 
 module.exports=router;
