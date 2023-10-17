c5450aef5bb90a3ce5a9ac9ace1dbdf1ef5b4437
DaniBacheva <y.bacheva@abv.bg>
2023-10-17 20:20:14 +0300

    render profile, create page

--- src/controllers/allPostController.js
+++ src/controllers/allPostController.js
@@ -4,4 +4,12 @@ router.get('/', (req,res) => {
     res.render('posts/all-posts')
 });
 
+router.get('/create', (req,res)=> {
+    res.render("posts/create")
+});
+
+router.get("/profile", (req,res) => {
+    res.render("posts/profile")
+});
+
 module.exports = router;
