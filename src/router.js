52b4e47966c2427c7ea60887f9a78f604d5983a3
DaniBacheva <y.bacheva@abv.bg>
2023-10-17 20:10:50 +0300

    Add all post page

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
-const userController = require('./controllers/userController')
+const userController = require('./controllers/userController');
+const allPostController = require('./controllers/allPostController');
 
 router.use(homeController);
 router.use("/users", userController);
+router.use("/posts", allPostController);
 
 router.get("*", (req,res)=> {
     res.redirect("/404");
