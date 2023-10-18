5256f658763a534435c3bf43e2e378775ec786ec
DaniBacheva <y.bacheva@abv.bg>
2023-10-18 20:20:44 +0300

    buttons in details

--- src/controllers/allPostController.js
+++ src/controllers/allPostController.js
@@ -1,44 +1,47 @@
 const router = require("express").Router();
 const creatureManager = require('../managers/creatureManager')
 
-router.get('/', async  (req,res) => {
-    const creatures =await creatureManager.getAll().lean();
-    console.log({creatures})
-    res.render('posts/all', {creatures});
+router.get('/', async (req, res) => {
+    const creatures = await creatureManager.getAll().lean();
+    console.log({ creatures })
+    res.render('posts/all', { creatures });
 });
 
-router.get('/create', (req,res)=> {
+router.get('/create', (req, res) => {
     res.render("posts/create")
 });
 
-router.get("/profile", (req,res) => {
+router.get("/profile", (req, res) => {
     res.render("posts/profile")
 });
 
-router.post("/create", async (req,res)=> {
-   const { 
-    name,
-    species,
-    skinColor,
-    eyeColor,
-    image,
-    description  } = req.body;
+router.post("/create", async (req, res) => {
+    const {
+        name,
+        species,
+        skinColor,
+        eyeColor,
+        image,
+        description } = req.body;
 
-    const payload = { name, species, skinColor, eyeColor, image, description , owner: req.user };
+    const payload = { name, species, skinColor, eyeColor, image, description, owner: req.user };
 
     await creatureManager.create(payload);
 
     console.log(req.body)
-     res.redirect ('/posts/')
+    res.redirect('/posts/')
 });
 
-router.get('/:creatureId/details', async (req,res)=> {
+router.get('/:creatureId/details', async (req, res) => {
 
     const creatureId = req.params.creatureId;
- 
     const creature = await creatureManager.getSingleCreature(creatureId).lean();
-       console.log({creature});
-    res.render(`posts/details`, { creature})
+    const { user } = req;
+    const { owner } = creature;
+    const isOwner = user?._id===owner.toString()
+
+        console.log({ creature });
+    res.render(`posts/details`, { creature, isOwner })
 })
 
 module.exports = router;
